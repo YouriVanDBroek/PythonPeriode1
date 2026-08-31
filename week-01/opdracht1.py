eef77ba67ea2492ac3112c1eb1b958712a47b60d
Youri van den Broek <yibk@mboutrecht.nl>
2026-08-31 13:53:24 +0200

    Eerste test opdracht

---
+++ week-01/opdracht1.py
@@ -0,0 +1,8 @@
+# oefening 1
+# Verander de waarde van het variabel "naam" naar jou naam
+# Voeg daaronder een nieuw variabel "leeftijd" toe en geef deze de waarde van jouw leeftijd
+# Print daarna deze waarde uit in een zin
+
+naam = ""
+
+print()
